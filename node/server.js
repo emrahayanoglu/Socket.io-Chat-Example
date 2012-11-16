27be3e2df44a85b633d0b1ad46b6395c7a038767
Emrah Ayanoglu <emrahayanoglu@gmail.com>
2012-11-16 17:26:55 +0200

    updated

--- node/server.js
+++ node/server.js
@@ -1,10 +1,14 @@
 var io = require('socket.io').listen(8089);
 
-console.log("Hello World");
-
 io.configure(function(){
 	io.set('transports', ['websocket', 'flashsocket']);
 });
+
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
 
 var userList = new Array();
 var User = function(){
@@ -31,6 +35,18 @@ var sendToOthers = function(userArray,id,nickname,msg){
 		}
 	}
 };
+var sendToAll = function(userArray,nickname,msg,typeValue){
+	for(var i = 0;i < userArray.length; i++){
+		io.sockets.sockets[userArray[i].id].json.send({type:typeValue,chatMsg:msg,sender:nickname});
+	}
+};
+var removeUser = function(userArray,user){
+	for(var i = 0;i < userArray.length; i++){
+		if(userArray[i].id === user.id){
+			userArray.remove(i);
+		}
+	}
+};
 
 io.sockets.on('connection', function(socket){
 	console.log("A user is connected");
@@ -53,16 +69,21 @@ io.sockets.on('connection', function(socket){
 		console.log(userList);
 
 		socket.json.send({type:10,msg:"Nickname setting is finished"});
+		sendToAll(userList,newUser.nickname,userList,11);
 	});
 	socket.on('sendChat',function(data){
 		var user = findUser(userList,socket.id);
 		if(user != null){
 			console.log("A message is received: " + data);
 			console.log("Message received from: " + user.toString());
-			sendToOthers(userList,user.id,user.nickname,data);
+			sendToAll(userList,user.nickname,data.chatMsg,5);
 		}
 	});
 	socket.on('disconnect',function(){
 		console.log("User is disconnected");
+		//Disconnect from server
+		var user = findUser(userList,socket.id);
+		removeUser(userList,user);
+		sendToAll(userList,"",userList,11);
 	});
 });
